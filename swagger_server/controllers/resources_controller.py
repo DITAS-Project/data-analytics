753773b19c6e329b1291c488dcfe961d39a7e3dd
Bogdan Despotov <bogdan.despotov@cloudsigma.com>
2019-07-09 13:26:19 +0300

    Further fixing the resource capacity API call and changing the logic for the usage

--- swagger_server/controllers/resources_controller.py
+++ swagger_server/controllers/resources_controller.py
@@ -41,15 +41,16 @@ def current_usage(infraId, nodeId=None):  # noqa: E501
     total_cpu_usage = 0
     total_cpu_cap = 0
     for node in res.items:
-        if nodeId and node.metadata.name == nodeId:
-            cpu = util.normalize_metrics(cores=int(node.status.capacity['cpu']), cpu=result['usage']['cpu'])['cpu']
+        if nodeId:
+            if node.metadata.name == nodeId:
+                cpu = util.normalize_metrics(cores=int(node.status.capacity['cpu']), cpu=result['usage']['cpu'])['cpu']
         elif node.metadata.name in config['infra'][infraId].keys():
             for n in result['items']:
                 if n['metadata']['name'] == node.metadata.name:
                     cpu = util.normalize_metrics(cores=int(node.status.capacity['cpu']), cpu=n['usage']['cpu'])['cpu']
-                    total_cpu_usage += config['infra'][infraId][node.metadata.name]['cpu'] * (cpu / 100)
+                    total_cpu_usage += (config['infra'][infraId][node.metadata.name]['cpu'] * (cpu / 100))
                     total_cpu_cap += config['infra'][infraId][node.metadata.name]['cpu']
-                    total_mem_usage += util.normalize_metrics(mem=result['usage']['memory'])['mem']
+                    total_mem_usage += util.normalize_metrics(mem=n['usage']['memory'])['mem']
 
     if total_cpu_usage and total_cpu_cap:
         total_cpu_average = total_cpu_usage / total_cpu_cap
@@ -81,12 +82,17 @@ def resources(infraId, nodeId=None):  # noqa: E501
         result = v1.list_node()
     except Exception as e:
         return 'Cannot connect to Kubernetes API: {}'.format(e), 500
-
+    cpu = 0
+    mem = 0
+    stor = heketi_client.get_storage_metrics(substring='size')
+    storage = util.normalize_metrics(storage=stor['total_bytes_size'])['storage']
     for node in result.items:
-        if node.metadata.name == nodeId:
-            mem = util.normalize_metrics(mem=node.status.capacity['memory'])['mem']
-            stor = heketi_client.get_storage_metrics(substring='size')
-            storage = util.normalize_metrics(storage=stor['total_bytes_size'])['storage']
-            return {'cpu': node.status.capacity['cpu'], 'mem': mem, 'storage': storage}, 200
+        if nodeId:
+            if node.metadata.name == nodeId:
+                mem = util.normalize_metrics(mem=node.status.capacity['memory'])['mem']
+                cpu = config['infra'][infraId][node.metadata.name]['cpu']
+        elif node.metadata.name in config['infra'][infraId].keys():
+            mem += util.normalize_metrics(mem=node.status.capacity['memory'])['mem']
+            cpu += config['infra'][infraId][node.metadata.name]['cpu']
 
-    return 'Node {} not found'.format(nodeId), 404
+    return {'cpu': cpu, 'mem': mem, 'storage': storage}, 200
